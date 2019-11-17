47b4b843f64bd550b8dc9a0c51b1cf932e4d700f
Thomas Richards <Deskbot@users.noreply.github.com>
2019-11-17 14:08:22 +0000

    Add car

--- src/Game.svelte
+++ src/Game.svelte
@@ -106,6 +106,22 @@
 	{:else if currentLevelNum === 2}
 		<Level
 			carDef={[
+				[1,2, 2, 1],
+				[3,0, 1, 3],
+				[0,3, 2, 1]
+			]}
+			bind:offset={middle}
+			rows={5}
+			cols={5}
+			cellsToClear={[
+				[1,0], [1,1], [1,2], [1,3], [1,4],
+				[2,0], [2,1], [2,2], [2,3], [2,4],
+			]}
+			startCell={[3,4]}
+		/>
+	{:else if currentLevelNum === 3}
+		<Level
+			carDef={[
 				[0,0, 1, 2],
 				[1,0, 2, 1],
 				[3,0, 2, 1],
