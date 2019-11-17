061885da3e79f9b362684f32ccd5f698dd1e1a71
Thomas Richards <Deskbot@users.noreply.github.com>
2019-11-17 15:10:54 +0000

    Add level

--- src/Game.svelte
+++ src/Game.svelte
@@ -112,6 +112,21 @@
 	{:else if currentLevelNum === 2}
 		<Level
 			carDef={[
+				[2,2, 2, 1],
+				[1,0, 1, 3],
+			]}
+			bind:bounds={bounds}
+			rows={4}
+			cols={5}
+			cellsToClear={[
+				[1,0], [1,1], [1,2], [1,3], [1,4],
+				[2,0], [2,1], [2,2], [2,3], [2,4],
+			]}
+			startCell={[3,3]}
+		/>
+	{:else if currentLevelNum === 3}
+		<Level
+			carDef={[
 				[1,2, 2, 1],
 				[3,0, 1, 3],
 				[0,3, 2, 1]
